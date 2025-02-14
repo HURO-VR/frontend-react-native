f1d723330e464535a4cfb76cfbb07900f1ca287f
nikita-x1 <accounts@nikitax.io>
2025-02-14 09:19:40 -0800

    CreateOrganization adds to user Orgs

--- firebase_CLI/functions/organization.js
+++ firebase_CLI/functions/organization.js
@@ -1,20 +1,26 @@
 import * as functions from "firebase-functions";
-import { FieldValue, getFirestore, initializeFirestore } from "firebase-admin/firestore";
+import { FieldValue, getFirestore } from "firebase-admin/firestore";
 import { initializeApp } from "firebase-admin/app";
 
 export const OrganizationMemberUpdate = functions.firestore.onDocumentWritten("organizations/{orgID}", (event) => {
     const before = event.data?.before.exists ? event.data.before.data() : null;
     const after = event.data?.after.exists ? event.data.after.data() : null;
-    const params = event.param
+    const params = event.params
+
     initializeApp()
-    const fs = initializeFirestore()
+    const fs = getFirestore()
     const batch = fs.batch()
 
-    if ((before && after && before.members.length < after.members.length) || after.members.length > 1) {
-        const newMembers = (after.members).filter((newMember) => {
-            return (before.members).find(currMember => currMember == newMember) == undefined
-        })
-        
+    if ((before && after && before.members.length < after.members.length) || (!before && after.members.length > 1)) {
+        let newMembers
+        if (before) {
+            newMembers = (after.members).filter((newMember) => {
+                return (before.members).find(currMember => currMember == newMember) == undefined
+            })
+        }
+        else {
+            newMembers = after.members
+        }
         
         newMembers.forEach((memberID) => {
             batch.update(fs.doc(`users/${memberID}`), {
