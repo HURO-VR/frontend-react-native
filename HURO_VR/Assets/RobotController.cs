d35a658c1df5878846cf3e7f3da977bc9879dc75
nikita-x1 <accounts@nikitax.io>
2025-02-25 17:56:04 -0800

    Robot stop on goal

--- HURO_VR/Assets/RobotController.cs
+++ HURO_VR/Assets/RobotController.cs
@@ -7,11 +7,45 @@ public class RobotController : MonoBehaviour
 
     public GameObject goal;
     public float maxVelocity;
+    bool goalReached = false;
+    Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (goal == null)
+        {
+            InitGoal(); 
+        }
+        body = GetComponent<Rigidbody>();
+        if (maxVelocity == 0) maxVelocity = 2;
+    }
+
+    public void SetVelocity(float x, float z)
+    {
+        if (!goalReached) body.velocity = new Vector3(x, 0, z);
+    }
+
+    public void GoalReached()
+    {
+        goalReached = true;
+        body.velocity = Vector3.zero;
+        body.isKinematic = true;
+    }
+
+    void InitGoal()
+    {
+        GoalController[] goals = FindObjectsByType<GoalController>(FindObjectsSortMode.InstanceID);
+        foreach (GoalController goal in goals)
+        {
+            if (!goal.HasRobot())
+            {
+                goal.SetRobot(gameObject);
+                this.goal = goal.gameObject;
+                return;
+            }
+        }
+        Debug.LogWarning("Robot " + gameObject.name + " does not have goal.");
     }
 
     // Update is called once per frame
