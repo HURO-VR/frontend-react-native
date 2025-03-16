bb813aaa5e8ddac4927545e23002e7c296a3f407
nikita-x1 <accounts@nikitax.io>
2025-03-16 12:25:21 -0700

    default script template

---
+++ HURO_VR/Assets/Scripts/SceneData/Classes/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    
+    #region Public Variables
+    // Add public variables here
+    #endregion
+
+    #region Private Variables
+    // Add private variables here
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        // Initialization code
+    }
+
+    private void Update()
+    {
+        // Per-frame logic
+    }
+    #endregion
+
+    #region Public Methods
+    // Add public methods here
+    #endregion
+
+    #region Private Methods
+    // Add private methods here
+    #endregion
+}
